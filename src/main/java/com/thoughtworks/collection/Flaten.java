dc872bc6612395cf3f86b237f6b8ec818945c735
Andrea <Andrea2000728>
2023-02-09 16:28:56 +0800

    feat: implement Flaten class and pass all the test

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,26 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Stream.of(array).flatMap(Stream::of).collect(Collectors.toList());
+
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = Arrays.stream(array).flatMap(Stream::of).collect(Collectors.toList());
+        return result.stream().distinct().collect(Collectors.toList());
+
     }
 }
